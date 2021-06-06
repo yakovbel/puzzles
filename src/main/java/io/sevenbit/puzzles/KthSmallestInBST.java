27da363628004541c8426945e5136a080eeaa1af
yakovbel <andrey.belyakov@lifestreet.com>
2021-06-06 23:43:57 +0300

    LeetCode 101. Symmetric Tree

--- src/main/java/io/sevenbit/puzzles/KthSmallestInBST.java
+++ src/main/java/io/sevenbit/puzzles/KthSmallestInBST.java
@@ -82,4 +82,19 @@ public class KthSmallestInBST {
         }
     }
 
+    public int test(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+
+            root = stack.removeLast();
+            if (--k == 0) return root.val;
+            root = root.right;
+        }
+
+    }
+
 }
